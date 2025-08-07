1057c53c97c61ca02909572b41ccdf7722eff8e0
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-MacBook-Air.local>
2025-08-06 20:59:52 -0400

    build a basic llm with lcel

---
+++ 3-Basic-App-LCEL/serve.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from langserve import add_routes
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from fastapi.routing import APIRoute
+
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+model = ChatGroq(model="gemma2-9b-it", groq_api_key=groq_api_key)
+
+# 1. Create prompt template
+system_template = "Translate the following into {language}"
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", system_template),
+    ("user", "{text}")
+])
+
+parser = StrOutputParser()
+
+# Define input schema for the chain
+class TranslationInput(BaseModel):
+    language: str
+    text: str
+
+
+# create chain
+chain = prompt_template | model | parser
+
+## App definition
+app = FastAPI(title="This is my langchain server", version="1.0", description="Simple API Server running langchain interfaces")
+
+add_routes(app, chain, path="/chain")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
